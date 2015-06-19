96869a386b49c5fe43412da188f7c27e1dd4cf57
dachaja <pettitbusiness@gmail.com>
2015-06-19 14:01:46 -0700

    added operator.

---
+++ java8fundamentals/app/anna/p/exercise/Operator.java
@@ -0,0 +1,48 @@
+package anna.p.exercise;
+
+public class Operator {
+	public static void main(String[] args) {
+//		int num1 = 1, num2 =1;
+//		
+//		for (int i = 0; i < 10; i++) {
+//			num1 = i;
+//			System.out.println("num1++ " + num1++);
+//			System.out.println("after num1++ " + num1);
+//		}
+//		
+//		for (int i = 0; i < 10; i++) {
+//			num2 = i;
+//			System.out.println("++num2 " + ++num2);
+//		}
+		additionOperator();
+		
+	}
+	
+	private static void additionOperator() {
+		byte b1;
+		byte b2 = 2;
+		byte b3 = 3;
+		b1 = (byte) (b2 + b3);
+		b1 = (byte) ((byte) b2 + b3);
+		b1 = 3 + 2;
+		
+		b1 = 2;
+		b2 = 3;
+		short s1 = 100;
+		int i = 10;
+		int j = 12;
+		float f1 = 2.5F;
+		double d1 = 20.0;
+		
+		b1 = 15 + 110;
+		b1 = (byte) (127 + 100);
+		
+		System.out.println("b1 = " + b1);
+		
+		b1 = 15 + 110;
+		b1 = (byte) (i + 5);
+		
+		String str2 = "Hello";
+		String str1 = i + j + str2;
+	}
+}
--- java8fundamentals/app/anna/p/exercise/Test.java
+++ java8fundamentals/app/anna/p/exercise/Test.java
@@ -1,5 +1,21 @@
 package anna.p.exercise;
 
 public class Test {
+	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++) {
+			int num1 = i, num2 = i;
+			System.out.println("num1++ " + num1++);
+			System.out.println("++num2 " + ++num2);
+			
+			num1 = num2 = 25;
+			System.out.println("num1 num2 " + num1 + num2);
+		}
+		
+		byte b1;
+		byte b2=2;
+		byte b3=3;
+		
+		//b1=127+10;
+	}
 
 }
