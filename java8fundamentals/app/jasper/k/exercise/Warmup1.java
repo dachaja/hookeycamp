601d9e00166fca09c681a83a462729efe6c05de0
JasperKONG <wjdgusrhd@naver.com>
2015-06-19 14:35:06 -0700

    CodingBat,Add,else

--- java8fundamentals/app/jasper/k/exercise/Warmup1.java
+++ java8fundamentals/app/jasper/k/exercise/Warmup1.java
@@ -4,20 +4,23 @@ public class Warmup1 {
 	public static boolean sleepIn(boolean weekday, boolean vacation) {
 		if (weekday == true) {
 			if (vacation == true) {
+				boolean sleepIn;
+				return sleepIn = true;
+			} else {
+				boolean sleepIn;
+				return sleepIn = false;
+
+			}
+		} else {
+			if (vacation == true) {
+				boolean sleepIn;
+				return sleepIn = true;
 			} else {
 				boolean sleepIn;
 				return sleepIn = false;
 			}
-		} else {
-			boolean sleepIn;
-			return sleepIn = false;
 		}
-		boolean sleepIn;
-		return sleepIn = true;
 	}
 	public static void main(String[] args) {
-		sleepIn(true,true);
-		String sleepIn = "sleep";
-		System.out.println(" Weekday + Vacation = " +  sleepIn);
 	}
 }
