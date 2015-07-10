1ce43641204cbfb2a02aa15cbb20fa392d2cd0a4
JasperKONG <wjdgusrhd@naver.com>
2015-07-10 10:18:01 -0700

    AddMoreIfElseEx4

---
+++ java8fundamentals/app/jasper/k/exercise/IfElseEx4.java
@@ -0,0 +1,14 @@
+package jasper.k.exercise;
+
+public class IfElseEx4 {
+	public static void main(String[] args) {
+		String title = "";
+		boolean isMale = true;
+		if (isMale) {
+			title = "Mr.";
+		} else {
+			title = "Ms.";
+		}
+		System.out.println(title);
+	}
+}
