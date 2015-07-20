d63b08c0ccc7d8a8d6c2fece5f72fc75d44bb4e8
JasperKONG <wjdgusrhd@naver.com>
2015-07-20 11:08:18 -0700

    WhileAddMore

--- java8fundamentals/app/jasper/k/exercise2/BreakEx4.java
+++ java8fundamentals/app/jasper/k/exercise2/BreakEx4.java
@@ -1,5 +1,20 @@
 package jasper.k.exercise2;
 
 public class BreakEx4 {
-
+	public static void main(String[] args) {
+		lab1:
+		{
+			int i = 10;
+			if ( i == 10)
+				break lab1;
+		}
+		lab2 :
+		{
+			int i = 10;
+			if (i == 10)
+				// A compile-time error. lab1 cannot be used here because this block is not
+				// associated with lab1 label. We can use only lab2 in this block
+				break lab2;
+		}
+	}
 }
--- java8fundamentals/app/jasper/k/exercise2/FlowEx25.java
+++ java8fundamentals/app/jasper/k/exercise2/FlowEx25.java
@@ -1,5 +1,29 @@
 package jasper.k.exercise2;
 
 public class FlowEx25 {
-
+	public static void main(String[] args) {
+		int sum = 0;
+		int i = 0;
+		
+		while(true) {
+			if(sum > 100)
+				break;
+			++i;
+			 sum += 1;
+		} //end of while
+		System.out.println(" i= " + i);
+		System.out.println("sum= " + sum);
+		
+		// do-while
+		sum = 0;
+		i = 0;
+		do {
+			++i;
+			sum += i;
+		}while (sum >100);
+		// for
+		for (sum = 0; sum >100; sum+=i){
+			++i;
+		}
+	}
 }
