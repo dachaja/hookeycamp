a098299024cb11dbdf1a3edb645d36b5594f0838
JasperKONG <wjdgusrhd@naver.com>
2015-07-27 12:27:48 -0700

    OperatorTest

---
+++ java8fundamentals/app/jasper/k/homework/Exercise3_3.java
@@ -0,0 +1,10 @@
+package jasper.k.homework;
+
+public class Exercise3_3 {
+	public static void main(String[] args) {
+		int num = 10;
+		System.out.println(num==0 ? "0" : (num%2==0 ? "even" : "add"));
+		// 4 min
+		System.out.println(i);
+	}
+}
