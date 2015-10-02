69e413451ac50271470960cec87bc3a98cd74374
JasperKong <wjdgusrhd@naver.com>
2015-10-01 17:57:47 -0700

    modified:   java8fundamentals/app/jasper/k/ch9/DivideMyZero.java 	new file:   java8fundamentals/app/jasper/k/game/ImageLoader.java 	new file:   java8fundamentals/app/jasper/k/game/ImageLoadtest.java 	new file:   java8fundamentals/app/jasper/k/game/images/imageloader/angel1.gif 	new file:   java8fundamentals/app/jasper/k/game/images/imageloader/angel2.gif 	new file:   java8fundamentals/app/jasper/k/game/images/imageloader/angel3.gif 	new file:   java8fundamentals/app/jasper/k/game/images/imageloader/angel4.gif 	new file:   java8fundamentals/app/jasper/k/game/images/imageloader/angel5.gif 	new file:   java8fundamentals/app/jasper/k/game/images/imageloader/angel6.gif 	new file:   java8fundamentals/app/jasper/k/game/images/imageloader/angel7.gif 	new file:   java8fundamentals/app/jasper/k/game/images/imageloader/angel8.gif 	new file:   java8fundamentals/app/jasper/k/game/images/imageloader/angel9.gif 	new file:   java8fundamentals/app/jasper/k/game/images/imageloader/evil1.gif 	new file:   java8fundamentals/app/jasper/k/game/images/imageloader/evil2.gif 	new file:   java8fundamentals/app/jasper/k/game/images/imageloader/evil3.gif 	new file:   java8fundamentals/app/jasper/k/game/images/imageloader/evil4.gif 	new file:   java8fundamentals/app/jasper/k/game/images/imageloader/evil5.gif 	new file:   java8fundamentals/app/jasper/k/game/images/imageloader/evil6.gif 	new file:   java8fundamentals/app/jasper/k/game/images/imageloader/evil7.gif 	new file:   java8fundamentals/app/jasper/k/game/images/imageloader/evil8.gif 	new file:   java8fundamentals/app/jasper/k/game/images/imageloader/evil9.gif :wq

--- java8fundamentals/app/jasper/k/ch9/DivideMyZero.java
+++ java8fundamentals/app/jasper/k/ch9/DivideMyZero.java
@@ -6,8 +6,14 @@ public class DivideMyZero {
 			int x = 10, y = 0, z;
 			z= x/y;
 			System.out.println("z= "+z);
-		} catch (Exception e){
+		} catch (ArithmeticException e){
+			// Get the description of the exception
+			String msg = e.getMessage();
 			
+			// Print a custom error message
+			System.out.println("An error has occured. The error is:" + msg);
 		}
+		
+		System.out.println("At the end of the program.");
 	}
 }
