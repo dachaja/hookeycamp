2e38ba147898bcb05b3b6e70c0192f1470202fde
dachaja <pettitbusiness@gmail.com>
2015-10-13 10:47:25 -0700

    added hello comment1

--- java8fundamentals/app/anna/p/ch2/Hello2.java
+++ java8fundamentals/app/anna/p/ch2/Hello2.java
@@ -1,8 +1,21 @@
 package anna.p.ch2;
 
 public class Hello2 {
+	/**
+	 * This method is...
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * */
+	// This is single comment.
+	/*
+	 * This is double comment.
+	 * */
 	public static void main(String[] args) {
 		System.out.println("Hello2");
+		Math.round(10);
 	}
 }
 class Hello33 { }
