ad6aeab0904caddb8b6500cef6c89d35231db3e1
sdrobinpark <sdrobinpark@gmail.com>
2015-12-19 21:01:56 -0800

    exercise

--- java8fundamentals/app/robin/p/homework/Exercise4_1.java
+++ java8fundamentals/app/robin/p/homework/Exercise4_1.java
@@ -8,15 +8,22 @@ public static void main(String[] args) {
 	
 	
 	char ch ='x';
+	if ((ch == ' ') || (ch=='\t')) {
+		
+	} else {
+		System.out.println("true");
+	}
 	boolean letter;
-	if (ch = 'x'|'X')
+	if ((ch == 'x')|(ch=='X'))
 	letter = true;
 	else
 	letter = false;
 	
-	char ch1 = 5;
+	char ch1 = '0';
 	boolean result1 = (ch1>0 && ch1<10)? true : false;
 	
+	
+
 	
 	System.out.println("Result =" + result);
 	System.out.println("Result1 =" + result1);
