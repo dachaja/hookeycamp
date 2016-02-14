4c175aac7efad63aea5c46512e1fa0f79f8bae4e
Ellyrmt <click373@gmail.com>
2016-02-13 19:45:11 -0800

    feb13_2016

---
+++ java8fundamentals/app/elly/c/ch5/SwitchEx2.java
@@ -0,0 +1,83 @@
+package elly.c.ch5;
+
+public class SwitchEx2 {
+	public static void main(String[] args) {
+		int age = 10;
+		
+		switch(age) {
+		case 10:
+			System.out.println("Ten");
+			break;
+		case 20:
+			System.out.println("Twenty");
+			default:
+		    	System.out.println("No-match");
+		}
+		
+		String process = "Wakeup";
+		
+		switch(process) {
+		case "Wakeup":
+			System.out.println("Step1");
+		case "Shower":
+			System.out.println("Step2");
+			break;
+		case "Breakfast":
+			System.out.println("Step3");
+			break;
+		}
+		
+		byte b = 10;
+		switch(b) {
+		case 5:
+			 b++;
+		case (byte) 150:
+			b--;
+			default:
+				 b = 0;
+		}
+		
+		int num = 10;
+		switch(num) {
+		case 10:
+			num++;
+		case 20:
+			num--;
+			default:
+				num = 100;
+		}
+		
+		int num1 = 10;
+		int num2 = 10;
+		switch(num1) {
+		case 20:
+			System.out.println("num1 is 20");
+		case 10:
+			System.out.println("num1 is 10");
+		}
+		
+		//Using an if-else statement
+		int i = 10;
+		if(i==10) {
+			System.out.println("i is 10");
+		}else if(i==20) {
+			System.out.println("i is 20");
+		}else{
+			System.out.println("i is neither 10 nor 20");
+		}
+		
+		//Using a switch statement
+		switch(i) {
+		case 10:
+			System.out.println("i is 10");
+			break;
+		case 20:
+			System.out.println("i is 20");
+			break;
+		default:
+			System.out.println("i is neither 10 nor 20");
+			break;
+		}
+		
+	}
+}
