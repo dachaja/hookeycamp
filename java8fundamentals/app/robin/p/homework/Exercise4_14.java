2e3b1e79906fea8f257ca772f51a8f517956d23d
dachaja <pettitbusiness@gmail.com>
2016-04-09 20:28:50 -0700

    updated homework

--- java8fundamentals/app/robin/p/homework/Exercise4_14.java
+++ java8fundamentals/app/robin/p/homework/Exercise4_14.java
@@ -3,7 +3,7 @@ package robin.p.homework;
 public class Exercise4_14 {
 
 	public static void main(String[] args){
-	int answer = 
+	int answer = (int) ((Math.random() * 100) + 1);
 	int input = 0;
 	int count = 0;
 	
@@ -13,11 +13,18 @@ public class Exercise4_14 {
 		count ++;
 		System.out.print("1과 100사이의 값을 입력하세요 :");
 		input = s.nextInt();
-			
-			
-	
+		
+		if(input > answer) {
+			System.out.println("더 작은 수를 입력하시오.");
+		} else if(input < answer) {
+			System.out.println("더 큰 수를 입력하시오.");
+		} else if(input == answer) {
+			System.out.println("맞췄습니다.");
+			System.out.println("시도 횟수는 " + count + "번입니다.");
+			break;
+		}
 	} while(true);
 	
-	}
+	} 
 }
 
