e8bf4a0f98dbaaebbe4423df9c0d32ec20eb3811
sdrobinpark <sdrobinpark@gmail.com>
2016-04-09 20:26:27 -0700

    exercise

--- java8fundamentals/app/robin/p/homework/Exercise4_14.java
+++ java8fundamentals/app/robin/p/homework/Exercise4_14.java
@@ -3,7 +3,7 @@ package robin.p.homework;
 public class Exercise4_14 {
 
 	public static void main(String[] args){
-	int answer = 
+	int answer = (int) ((Math.random() * 100) + 1); // 1-100까지 랜덤으로 수를 창조하는 
 	int input = 0;
 	int count = 0;
 	
@@ -14,6 +14,15 @@ public class Exercise4_14 {
 		System.out.print("1과 100사이의 값을 입력하세요 :");
 		input = s.nextInt();
 			
+		if(input > answer) {
+			System.out.println("더 작은 수를 입력하시오.");
+		} else if(input < answer) {
+			System.out.println("더 큰수를 입력하시오.");
+		} else if(input == answer) {
+			System.out.println("맞췄습니다.");
+			System.out.println("시도 회수는 " + count + "번 입니다. ");
+			break;
+		}
 			
 	
 	} while(true);
